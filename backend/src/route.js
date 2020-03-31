9c0de83eac03b1a2d6e86542f9e3d434303419b8
Italo Guglielmini <italo.guglielmini@gmail.com>
2020-03-31 20:50:00 -0300

    Adicionando Joi e Celebrate e tratativa de erros do backend

--- backend/src/route.js
+++ backend/src/route.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 const OngController = require('./controllers/OngControllers');
-const IncidentsController = require('./controllers/IncidentControllers');
 const ProfileController = require('./controllers/ProfileControllers');
 const SessionController = require('./controllers/SessionControllers');
+const IncidentsController = require('./controllers/IncidentControllers');
 const routes = express.Router();
 
 
 //Login
 routes.post('/sessions', SessionController.create);
-
 //Ongs
 routes.get('/ongs',OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 //Perfil
 routes.get('/profile', ProfileController.index);
 //Incidentes
